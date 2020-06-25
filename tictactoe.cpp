be72a674fb6eb9dc94f4319cfb8d23065b283d31
Shaswat <bhattaraishaswat@gmail.com>
2020-06-24 22:06:59 -0600

    drawBoard

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 char matrix[3][3] = {'1','2','3','4','5','6','7','8','9'};
+void drawBoard()
+{
+
+}
 
 int main()
 {
-
+    drawBoard();
     return 0;
 }
