6b54f581587bd5a41e6fcfb9a9c9e7b67eab4a26
Shaswat <bhattaraishaswat@gmail.com>
2020-07-07 20:54:16 -0600

    added option to play with another player

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 using namespace std;
 
+// Code to Play with AI
 class game{
 
 private:
@@ -133,9 +134,216 @@ public:
     }
 };
 
+
+// Code to play with another player
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
+int n;
+void Draw()
+{
+    system("cls");
+    cout << "Tic Tac Toe v1.0" << endl;
+    printf("\n-------------\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("|");
+        for (int j = 0; j < 3; j++)
+        {
+            printf(" %c |", matrix[i][j]);
+        }
+        
+        printf("\n-------------\n");
+    }
+}
+void Input()
+{
+    int a;
+    cout << "It's " << player << " turn. " <<"Press the number of the field: ";
+    cin >> a;
+ 
+    if (a == 1)
+    {
+        if (matrix[0][0] == '1')
+            matrix[0][0] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 2)
+    {
+        if (matrix[0][1] == '2')
+            matrix[0][1] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 3)
+    {
+        if (matrix[0][2] == '3')
+            matrix[0][2] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 4)
+    {
+        if (matrix[1][0] == '4')
+            matrix[1][0] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 5)
+    {
+        if (matrix[1][1] == '5')
+            matrix[1][1] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 6)
+    {
+        if (matrix[1][2] == '6')
+            matrix[1][2] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 7)
+    {
+        if (matrix[2][0] == '7')
+            matrix[2][0] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 8)
+    {
+        if (matrix[2][1] == '8')
+            matrix[2][1] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+    else if (a == 9)
+    {
+        if (matrix[2][2] == '9')
+            matrix[2][2] = player;
+        else
+        {
+            cout << "Field is already in use try again!" << endl;
+            Input();
+        }
+    }
+     
+}
+void TogglePlayer()
+{
+    if (player == 'X')
+        player = 'O';
+    else
+        player = 'X';
+}
+char Win()
+{
+    //first player
+    if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+        return 'X';
+    if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+        return 'X';
+    if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
+        return 'X';
+ 
+    if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
+        return 'X';
+    if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+        return 'X';
+    if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+        return 'X';
+ 
+    if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+        return 'X';
+    if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
+        return 'X';
+ 
+    //second player
+    if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+        return 'O';
+    if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+        return 'O';
+    if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+        return 'O';
+ 
+    if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+        return 'O';
+    if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+        return 'O';
+    if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+        return 'O';
+ 
+    if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+        return 'O';
+    if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
+        return 'O';
+ 
+    return '/';
+}
+
+
 int main( int argc, char* argv[] )
 {
-    srand(time(0));
-    game game1;
+    string choise;
+    cout << "Do you want to play with ai or another player? AI = AI, Another Player = B \n";
+    getline (cin, choise);
+    
+    if (choise == "AI" ){
+        printf("You are playing with AI.\n ");
+        srand(time(0));
+        game game1;
+    }else if (choise == "B"){
+        n = 0;
+        Draw();
+        while (1)
+        {
+            n++;
+            Input();
+            Draw();
+            if (Win() == 'X')
+            {
+                cout << "X wins!" << endl;
+                break;
+            }
+            else if (Win() == 'O')
+            {
+                cout << "O wins!" << endl;
+                break;
+            }
+            else if (Win() == '/' && n == 9)
+            {
+                cout << "It's a draw!" << endl;
+                break;
+            }
+            TogglePlayer();
+        }
+        system("pause");
+    }else{
+        printf("Choose a valid response.\n");
+    }
     return 0;
 }
